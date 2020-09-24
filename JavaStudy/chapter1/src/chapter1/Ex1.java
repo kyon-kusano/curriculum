5c729c062d53758976ca11ba586279547056466b
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-09-24 18:51:17 +0900

    課題提出テスト（mainメソッド・インスタンス・メソッド呼び出し）
    Main.javaの作成
    Study.javaの作成
    Main.classの作成
    Study.classの作成

--- JavaStudy/chapter1/src/chapter1/Ex1.java
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -1,10 +1,14 @@
 package chapter1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("hello java");
+		List<String> list = new ArrayList<String>();
+			list.add("Hello java")
+			System.out.println(list.get(1);
 
 	}
 
