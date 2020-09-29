44bfb3481b23f6887fdd55dcfe81bd94ad54ca93
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-09-29 22:14:03 +0900

    課題提出（チェックテスト）
    jspTest.jspの作成
    header.jspの作成
    footer.jspの作成
    style.cssの作成
    web.xmlの作成

--- JavaStudy/Chedk2/src/check/Check.java
+++ JavaStudy/Chedk2/src/check/Check.java
@@ -1,16 +1,12 @@
 package check;
 
-
-//import check.Pet;
-//import check.RobotPet;
 import constants.Constants;
-
 
 public class Check {
 
 	private static String firstName = "草野";
 	private static String lastName = "匡亮";
-	
+
 	private static void PrintName(String firstName, String lastName) {
 		System.out.println("printNameメソッド → "+ (firstName + lastName));
 	}
