f608c404ce1f5673202f6b5cb75d752317230e7b
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-10-05 22:34:17 +0900

    課題提出（4章チェックテスト）
    EmployeeController.javaの作成
    EmployeeService.javaの作成
    index.jspの作成
    index.cssの作成
    index.htmlの作成

---
+++ JavaStudy/Check4/src/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package controller;
+/**
+ * 社員情報管理コントローラー
+ */
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.EmployeeBean;
+import service.EmployeeService;
+
+//継承しているHttpSerbletクラスのdopost()メソッドによりPOST通信で
+//第1引数HttpServletRequestインタフェース＝HTTPリクエストに対応
+//第2引数HttpServletResponseインタフェース＝HTTPレスポンスに対応
+
+public class EmployeeController extends HttpServlet {
+ public void doPost(HttpServletRequest request, HttpServletResponse response)
+ throws ServletException, IOException {
+
+ try {
+  // 問① index.htmlから送信されたIDとPassWordの値を取得できるように修正しましょう。
+//	 HttpServletRequestインターフェースの、 requestオブジェクト を使用してパラメータを読み込みそれぞれの変数へ代入
+ String id = request.getParameter("id");
+ String password = request.getParameter("pass");
+
+ /*
+ * IDとPassWordと元に、社員情報を検索する関数の呼び出し、結果をJSPに渡す処理
+ * ※ EmployeeBeanとEmployeeServiceをimportするのを忘れないでください。
+ */
+
+  // 問② EmployeeServiceクラスをインスタンス化する。
+ EmployeeService srev = new EmployeeService();
+  // 問③ EmployeeBeanに、EmployeeServiceよりsearch関数を呼び出し、返り値を格納する。
+ EmployeeBean bean = srev.search(id, password);
+  // 問④ nullの部分に適切な引数をセットする。
+ request.setAttribute("EmployeeBean", bean);
+
+ } catch (Exception e) {
+ e.printStackTrace();
+ } finally {
+ ServletContext context = this.getServletContext();
+ RequestDispatcher dispatcher = context.getRequestDispatcher("/index.jsp");
+ dispatcher.forward(request, response);
+ }
+ }
+}
