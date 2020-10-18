efd976543ef9459a71818dc10301798d9785bc63
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-10-18 12:31:24 +0900

    課題提出（条件分岐・演算子）
    index.html
    main.js
    の作成

---
+++ JavaScriptStudy/JavaScript/1-3/main.js
@@ -0,0 +1,19 @@
+let a;
+let b;
+console.log(a !== 10);
+console.log(10 <= b <= 20 && b % 2 === 0 );
+
+let x = 10;
+if(10 <= x <= 20){
+  console.log("成功です");
+}else{
+  console.log("失敗です");
+}
+
+let y = 0;
+if(y % 2 === 0){
+  console.log("偶数です");
+}
+if(y % 2 !== 0){
+  console.log("奇数です");
+}
