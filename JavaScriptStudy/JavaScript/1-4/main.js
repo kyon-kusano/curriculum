3a99ff163ecf82c4f2d769b9f03dfbd1d31350a5
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-10-22 20:11:55 +0900

    課題提出（繰り返し処理）
    main.jsの修正

--- JavaScriptStudy/JavaScript/1-4/main.js
+++ JavaScriptStudy/JavaScript/1-4/main.js
@@ -2,18 +2,12 @@ let num = 1;
 while(num <= 100){
   if(num % 3 === 0 && num % 5 ===0){
     console.log("FizzBuzz!!");
-    num++;
-    continue;
   }else if(num % 3 === 0){
     console.log("Fizz!");
-    num++;
-    continue;
   }else if(num % 5 === 0){
     console.log("Bizz!");
-    num++;
-    continue;
   }else{
     console.log(num);
-    num++;
   }
+  num++;
 }
