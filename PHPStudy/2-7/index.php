7c9a554a131eb1e25f5da92b35a35f8a95af8453
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-11-07 14:29:28 +0900

    課題提出（配列、連想配列）訂正
    index.phpの訂正

--- PHPStudy/2-7/index.php
+++ PHPStudy/2-7/index.php
@@ -1,7 +1,7 @@
 <?php
-$color1 = ["red" => "赤", "blue" => "青", "green" => "緑"];
-$color2 = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"];
-var_dump($color1);
+$color = ["red" => "赤", "blue" => "青", "green" => "緑"];
+var_dump($color);
 echo '<br>';
-var_dump($color2);
+$color["yellow"] = "黄色";
+var_dump($color);
 ?>
