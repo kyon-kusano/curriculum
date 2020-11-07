74a577eef24776beee42308707d8ef104e7351ad
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-11-07 14:25:43 +0900

    課題提出（配列、連想配列）
    index.phpの作成

---
+++ PHPStudy/2-7/index.php
@@ -0,0 +1,7 @@
+<?php
+$color1 = ["red" => "赤", "blue" => "青", "green" => "緑"];
+$color2 = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"];
+var_dump($color1);
+echo '<br>';
+var_dump($color2);
+?>
