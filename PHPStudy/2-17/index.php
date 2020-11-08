d96504d65b9d6167d0fca03821c9ad99f247b0bf
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-11-08 17:27:06 +0900

    課題提出（EXTRA問題）
    index.phpの作成

---
+++ PHPStudy/2-17/index.php
@@ -0,0 +1,27 @@
+<?php
+$sum = 0;
+$num = 1;
+while($sum < 40){
+  $rand = mt_rand(1, 6);
+  echo $num."回目＝".$rand.'<br>';
+  $sum += $rand;
+  $num++;
+}
+echo "合計".$num."回でゴールしました。";
+echo '<br><br>';
+?>
+
+
+<?php
+date_default_timezone_set('Asia/Tokyo');
+$time_s = date("H", time());
+$time = intval($time_s);
+echo "今".$time."時間台です<br>";
+if(4 <= $time && $time <= 11){
+  echo "おはようございます";
+}elseif(12 <= $time && $time<= 18){
+  echo "こんにちは";
+}else{
+  echo "こんばんは";
+}
+?>
