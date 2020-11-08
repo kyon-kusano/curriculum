c8bb735053aa4215b6e7c24698822aabfc70d3cb
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-11-08 12:29:18 +0900

    課題提出（いろいろなフォームの入力欄）
    index.php
    result.php
    の作成

---
+++ PHPStudy/2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $item = $_POST['item'];
+    $num = $_POST['num'];
+    ?>
+    <p>お名前：<?php echo $my_name; ?></p>
+    <p>ご希望商品：<?php echo $item; ?></p>
+    <p>個数：<?php echo $num; ?></p>
